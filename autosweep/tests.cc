836accad2eb9b955b2122c585d2cc0de1b36097f
Daniel Oom <oom.daniel@gmail.com>
2022-02-05 13:09:31 +0100

    Setup catch2 test framework

--- autosweep/tests.cc
+++ autosweep/tests.cc
@@ -1,65 +1,68 @@
 #include "autosweep/board.h"
 #include "autosweep/stream.h"
 
-#include <iostream>
-
 #include <opencv4/opencv2/opencv.hpp>
 
-void FromString_ExampleInput_ExpectedBoard() {
-  std::string string("-1\n"
-                     "10\n");
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-  Board board = Board::FromString(string);
+#include <iostream>
 
-  assert(board.rows() == 2);
-  assert(board.cols() == 2);
-  assert(board.at(0, 0).value == CELL_UNKNOWN);
-  assert(board.at(0, 1).value == 1);
-  assert(board.at(1, 0).value == 1);
-  assert(board.at(1, 1).value == 0);
+TEST_CASE("FromString", "[Board]") {
+  SECTION("example input, expected output") {
+    std::string string("-1\n"
+                       "10\n");
+
+    Board board = Board::FromString(string);
+
+    REQUIRE(board.rows() == 2);
+    REQUIRE(board.cols() == 2);
+    REQUIRE(board.at(0, 0).value == CELL_UNKNOWN);
+    REQUIRE(board.at(0, 1).value == 1);
+    REQUIRE(board.at(1, 0).value == 1);
+    REQUIRE(board.at(1, 1).value == 0);
+  }
 }
 
-void Test1() {
-  Board board = Board::FromString("-1\n"
-                                  "10\n");
+TEST_CASE("Changes", "[Board]") {
+  SECTION("one unknown cell with neighboring ones, gives new flag") {
+    Board board = Board::FromString("-1\n"
+                                    "10\n");
 
-  std::unordered_set<Cell> new_flags, new_clicks;
-  Changes(board, &new_flags, &new_clicks);
+    std::unordered_set<Cell> new_flags, new_clicks;
+    Changes(board, &new_flags, &new_clicks);
 
-  assert(new_flags.size() == 1);
-  assert(new_flags.cbegin()->row == 0 && new_flags.cbegin()->col == 0);
-  assert(new_clicks.empty());
+    REQUIRE(new_flags.size() == 1);
+    REQUIRE(new_flags.cbegin()->row == 0);
+    REQUIRE(new_flags.cbegin()->col == 0);
+    REQUIRE(new_clicks.empty());
+  }
+
+  SECTION("case where algorithm does not work, no clicks or flags") {
+    Board board = Board::FromString("------\n"
+                                    "--111-\n"
+                                    "-2101-\n"
+                                    "--212-\n"
+                                    "---1--\n"
+                                    "------\n");
+
+    std::unordered_set<Cell> new_flags, new_clicks;
+    Changes(board, &new_flags, &new_clicks);
+
+    REQUIRE(new_flags.empty());
+    REQUIRE(new_clicks.empty());
+  }
 }
 
-void Test2() {
-  Board board = Board::FromString("--1\n"
-                                  "--2\n"
-                                  "12f\n");
+TEST_CASE("ACellWithMostNeighboringMines", "[Board]") {
+  SECTION("example input, expected cell") {
+    Board board = Board::FromString("--1\n"
+                                    "--2\n"
+                                    "12f\n");
 
-  Cell cell = ACellWithMostNeighboringMines(board);
+    Cell cell = ACellWithMostNeighboringMines(board);
 
-  assert(cell.row == 1 && cell.col == 1);
-}
-
-void Test3() {
-  Board board = Board::FromString("------\n"
-                                  "--111-\n"
-                                  "-2101-\n"
-                                  "--212-\n"
-                                  "---1--\n"
-                                  "------\n");
-
-  std::unordered_set<Cell> new_flags, new_clicks;
-  Changes(board, &new_flags, &new_clicks);
-
-  assert(new_flags.empty());
-  assert(new_clicks.empty());
-}
-
-int main() {
-  FromString_ExampleInput_ExpectedBoard();
-  Test1();
-  Test2();
-  Test3();
-  return 0;
+    REQUIRE(cell.row == 1);
+    REQUIRE(cell.col == 1);
+  }
 }
