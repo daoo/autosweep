388b18db1159fa935d43ea990a50ce65c41b7919
Daniel Oom <oom.daniel@gmail.com>
2022-04-15 10:12:18 +0200

    Re-introduce commented tests

--- autosweep/network_unittests.cc
+++ autosweep/network_unittests.cc
@@ -4,41 +4,41 @@
 
 #include "autosweep/network.h"
 
-/* TEST_CASE("Network::Network", "[Neural]") { */
-/*   Network network({2, 3, 1}); */
+TEST_CASE("Network::Network", "[Neural]") {
+  Network network({2, 3, 1});
 
-/*   REQUIRE(network.biases().size() == 2); */
-/*   REQUIRE(network.biases().at(0).size() == cv::Size2i{1, 3}); */
-/*   REQUIRE(network.biases().at(1).size() == cv::Size2i{1, 1}); */
+  REQUIRE(network.biases().size() == 2);
+  REQUIRE(network.biases().at(0).size() == cv::Size2i{1, 3});
+  REQUIRE(network.biases().at(1).size() == cv::Size2i{1, 1});
 
-/*   REQUIRE(network.weights().size() == 2); */
-/*   REQUIRE(network.weights().at(0).size() == cv::Size2i{2, 3}); */
-/*   REQUIRE(network.weights().at(1).size() == cv::Size2i{3, 1}); */
-/* } */
+  REQUIRE(network.weights().size() == 2);
+  REQUIRE(network.weights().at(0).size() == cv::Size2i{2, 3});
+  REQUIRE(network.weights().at(1).size() == cv::Size2i{3, 1});
+}
 
-/* TEST_CASE("Network::FeedForward", "[Neural]") { */
-/*   Network network({2, 3, 1}); */
-/*   cv::Mat input({0.0, 1.0}); */
+TEST_CASE("Network::FeedForward", "[Neural]") {
+  Network network({2, 3, 1});
+  cv::Mat input({0.0, 1.0});
 
-/*   cv::Mat result = network.FeedForward(input); */
+  cv::Mat result = network.FeedForward(input);
 
-/*   REQUIRE(result.size() == cv::Size2i{1, 1}); */
-/* } */
+  REQUIRE(result.size() == cv::Size2i{1, 1});
+}
 
-/* TEST_CASE("Network::BackPropagate", "[Neural]") { */
-/*   Network network({2, 3, 1}); */
-/*   cv::Mat input({0.0, 1.0}); */
-/*   cv::Mat output({1.0}); */
+TEST_CASE("Network::BackPropagate", "[Neural]") {
+  Network network({2, 3, 1});
+  cv::Mat input({0.0, 1.0});
+  cv::Mat output({1.0});
 
-/*   auto result = network.BackPropagate(input, output); */
+  auto result = network.BackPropagate(input, output);
 
-/*   REQUIRE(result.b.size() == 2); */
-/*   REQUIRE(result.b[0].size() == cv::Size2i{1, 3}); */
-/*   REQUIRE(result.b[1].size() == cv::Size2i{1, 1}); */
-/*   REQUIRE(result.w.size() == 2); */
-/*   REQUIRE(result.w[0].size() == cv::Size2i{2, 3}); */
-/*   REQUIRE(result.w[1].size() == cv::Size2i{3, 1}); */
-/* } */
+  REQUIRE(result.b.size() == 2);
+  REQUIRE(result.b[0].size() == cv::Size2i{1, 3});
+  REQUIRE(result.b[1].size() == cv::Size2i{1, 1});
+  REQUIRE(result.w.size() == 2);
+  REQUIRE(result.w[0].size() == cv::Size2i{2, 3});
+  REQUIRE(result.w[1].size() == cv::Size2i{3, 1});
+}
 
 TEST_CASE("Network::Update", "[Neural]") {
   SECTION("2 layers with 1 neuron") {
