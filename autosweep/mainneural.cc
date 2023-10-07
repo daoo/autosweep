c52cc1313bbccf89a3fcecb5499bf0f19ab0d2ef
Daniel Oom <oom.daniel@gmail.com>
2023-10-07 15:58:31 +0200

    Print statistics in mainnerual

--- autosweep/mainneural.cc
+++ autosweep/mainneural.cc
@@ -12,23 +12,24 @@ namespace {
 
 void LeftClick(
     const Desktop& desktop, const BoardLocation& location, Cell cell) {
-  std::cout << "LeftClick(" << cell.row << ", " << cell.col << ")\n";
+  /* std::cout << "LeftClick(" << cell.row << ", " << cell.col << ")\n"; */
   desktop.LeftClick(location.CellCenter(cell.row, cell.col));
 }
 
-void Inner(
+size_t Inner(
     const Desktop& desktop, const BoardLocation& location, Network& network,
     double eta) {
+  size_t clicks = 0;
   while (true) {
-    std::cout << "Taking screenshot of " << location.Board() << "\n";
+    /* std::cout << "Taking screenshot of " << location.Board() << "\n"; */
     Board board = ParseBoard(desktop.Capture(location.Board()));
     if (board.IsWon()) {
-      std::cout << "Victory!\n";
-      return;
+      /* std::cout << "Victory!\n"; */
+      return clicks;
     }
     if (board.IsLost()) {
-      std::cout << "Lost!\n";
-      return;
+      /* std::cout << "Lost!\n"; */
+      return clicks;
     }
 
     cv::Mat input = GenerateInputLayer(board);
@@ -39,8 +40,10 @@ void Inner(
     cv::minMaxLoc(
         result, &minVal, nullptr, &minLoc, nullptr, UnknownCellMask(board));
     Cell lowestRiskCell = board.at(minLoc.y, minLoc.x);
-    std::cout << "Lowest risk of mine at " << minLoc << " (" << minVal << ")\n";
+    /* std::cout << "Lowest risk of mine at " << minLoc << " (" << minVal <<
+     * ")\n"; */
     LeftClick(desktop, location, lowestRiskCell);
+    ++clicks;
 
     Board board2 = ParseBoard(desktop.Capture(location.Board()));
 
@@ -53,7 +56,7 @@ void Inner(
 int main() {
   Desktop desktop;
   cv::Rect display_rectangle = desktop.GetDisplayRectangle();
-  std::cout << "Taking screenshot of " << display_rectangle << "\n";
+  /* std::cout << "Taking screenshot of " << display_rectangle << "\n"; */
   cv::Mat initial_screenshot = desktop.Capture(display_rectangle);
   BoardLocation location = BoardLocation::Find(initial_screenshot);
 
@@ -62,9 +65,18 @@ int main() {
   size_t cells = board.rows() * board.cols();
   Network network({cells, cells, cells});
   constexpr double eta = 3;
+  size_t games = 0;
+  size_t total = 0;
   while (true) {
     desktop.LeftClick(location.SmileyCenter());
     LeftClick(desktop, location, board.at(board.rows() / 2, board.cols() / 2));
-    Inner(desktop, location, network, eta);
+    auto clicks = Inner(desktop, location, network, eta);
+
+    total += clicks;
+    ++games;
+    std::cout << "Game: " << games;
+    std::cout << ", Clicks: " << clicks;
+    std::cout << ", Avg: " << static_cast<double>(total) / games;
+    std::cout << '\n';
   }
 }
